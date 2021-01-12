e474f79afd69f6175df9853b6d92c64030555d8d
Seul Martins <suelmartins15@gmail.com>
2021-01-12 20:55:17 -0300

    inicio

--- index.php
+++ index.php
@@ -2,3 +2,15 @@
  echo "Seja bem vindo";
 
  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de login</title>
+</head>
+<body>
+    
+</body>
+</html>
