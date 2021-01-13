7031389166e59883d6e84515a77d07ca02e15df9
Seul Martins <suelmartins15@gmail.com>
2021-01-13 20:58:37 -0300

    files

--- index.php
+++ index.php
@@ -4,13 +4,15 @@
  ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de login</title>
 </head>
 <body>
-    
+    <?php
+
+    ?>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,5 @@
+<form method="POST">
+    <input type="text"  name="login">
+    <input type="submit" name="senha">
+    <input type="submit" name="acao" value="Enviar">
+</form>
