a5d98f2f05deed5a86884c63df7c49842bbc404d
Seul Martins <suelmartins15@gmail.com>
2021-01-13 21:00:59 -0300

    files

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <form method="POST">
     <input type="text"  name="login">
-    <input type="submit" name="senha">
+    <input type="password" name="senha">
     <input type="submit" name="acao" value="Enviar">
 </form>
