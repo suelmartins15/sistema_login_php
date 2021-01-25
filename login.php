88ac5bc8d876f8844b1ab3afd0bfff86df41adbd
Seul Martins <suelmartins15@gmail.com>
2021-01-25 20:38:52 -0300

    update

--- login.php
+++ login.php
@@ -1,21 +1,4 @@
-<?php 
-    session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de Login</title>
-</head>
-<body>
-    <?php 
-        if(!isset($_SESSION['login'])){
-            echo "chama pagina de login";
-        }else{
-            echo "vai para pagina home";
-        }
+<?php
+    
 
-    ?>
-</body>
-</html>
+?>
