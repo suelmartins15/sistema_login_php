b057e0772b97db93141023a53477b48af869c8d7
Seul Martins <suelmartins15@gmail.com>
2021-01-30 20:45:56 -0300

    sistema_login

---
+++ home.php
@@ -0,0 +1,1 @@
+<h2>seje bem vindo!</h2>
---
+++ login.php
@@ -0,0 +1,5 @@
+<form action="" method="POST">
+    <input type="text" name="login">
+    <input type="submit" value="senha">
+    <input type="button" name="acao" value="Enviar dados" >
+</form>
