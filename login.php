fb82c495fe1b241327b8e939ca22d1c0ff65b631
Seul Martins <suelmartins15@gmail.com>
2021-02-06 20:27:53 -0300

    pagina de login

--- login.php
+++ login.php
@@ -1,2 +1,5 @@
-<?php 
-    echo 'Seja bem vindo pagina de login';
+<form method="POST">
+    <input type="text" name="login" placeholder="Usuario">
+    <input type="password" name="senha" placeholder="senha">
+    <input type="submit" name="acao" value="Enviar dados">
+</form>
