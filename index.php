73a5c23d30886ec93c9e28eee1e3487e391df2bf
Seul Martins <suelmartins15@gmail.com>
2021-02-06 21:49:09 -0300

    estrutura

--- index.php
+++ index.php
@@ -11,6 +11,25 @@
 <body>
     <?php 
         if(!isset($_SESSION['login'])){
+            if(isset($_SESSION['acao'])){
+                $login = 'suel';
+                $senha = '123';
+
+                $loginForm = $_POST['login'];
+                $senhaForm = $_POST['senha'];
+
+                if($login == $loginForm && $senha == $senhaForm){
+                    echo 'login realizado com  sucesso!';
+                    $_SESSION['login'] = $login;
+                    header('Location: index.php');
+                }else{
+                    echo 'Dados invalidos';
+                    
+                }
+            }else{
+
+            }
+
             include 'login.php';
         }else{
             include 'home.php';
